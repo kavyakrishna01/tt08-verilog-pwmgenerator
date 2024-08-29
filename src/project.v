1eb71ffb76ed61b5ce2723b319a8e3e5fed187cb
kavyakrishna01 <159253604+kavyakrishna01@users.noreply.github.com>
2024-08-29 14:01:05 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,6 +17,7 @@ module tt_um_pwmgenerator (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
+  assign uo_out= ui_in + uio_in;
   assign uio_in = 8'b0;
   assign uio_out = 8'b0;
   assign uio_oe  = 8'b0;
