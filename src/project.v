07e688fd0016be3919ee8871e9a64ea7c3fb605d
kavyakrishna01 <159253604+kavyakrishna01@users.noreply.github.com>
2024-08-29 12:14:12 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,11 +17,21 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_in = 0;
   assign uio_out = 0;
   assign uio_oe  = 0;
+  assign [7:2] ui_in  = 0;
+  assign [7:1] uo_out   = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, rst_n, 1'b0};
+pwmgenerator m1
+ (
+     .clk(clk), // 100MHz clock input 
+     .increase_duty(ui_in[0]), // input to increase 10% duty cycle 
+     .decrease_duty(ui_in[1]), // input to decrease 10% duty cycle 
+     .PWM_OUT(uo_out[0]) // 10MHz PWM output signal 
+    );
+
 
 endmodule
