f190498ed5caf6c9a5ace534cf8844d110b750c1
kavyakrishna01 <159253604+kavyakrishna01@users.noreply.github.com>
2024-08-29 20:33:15 +0530

    Update pwmgenerator.v

--- src/pwmgenerator.v
+++ src/pwmgenerator.v
@@ -7,13 +7,8 @@ module pwmgenerator
 );
 
     reg [27:0] counter_debounce = 0;   // Counter for generating slow clock enable signals
-    wire slow_clk_enable;              // Slow clock enable signal for debouncing FFs
     reg [3:0] DUTY_CYCLE = 5;          // Initial duty cycle is 50%
     reg [3:0] counter_PWM = 0;         // Counter for creating 10MHz PWM signal
-
-    // Debounced signals
-    reg tmp1 = 0, tmp2 = 0, duty_inc = 0;
-    reg tmp3 = 0, tmp4 = 0, duty_dec = 0;
 
     // Generate a slower clock signal for debouncing (4Hz)
     always @(posedge clk) begin
@@ -23,33 +18,24 @@ module pwmgenerator
             counter_debounce <= counter_debounce + 1;
     end
 
-    assign slow_clk_enable = (counter_debounce == 0); // Slow clock enable signal is high for one cycle
+    wire slow_clk_enable = (counter_debounce == 0); // Slow clock enable signal is high for one cycle
 
     // Debouncing logic for increasing duty cycle
+    reg increase_debounced = 0;
+    reg decrease_debounced = 0;
+
     always @(posedge clk) begin
         if (slow_clk_enable) begin
-            tmp1 <= increase_duty;
-            tmp2 <= tmp1;
+            increase_debounced <= increase_duty;
+            decrease_debounced <= decrease_duty;
         end
     end
-
-    assign duty_inc = tmp1 & (~tmp2) & slow_clk_enable;
-
-    // Debouncing logic for decreasing duty cycle
-    always @(posedge clk) begin
-        if (slow_clk_enable) begin
-            tmp3 <= decrease_duty;
-            tmp4 <= tmp3;
-        end
-    end
-
-    assign duty_dec = tmp3 & (~tmp4) & slow_clk_enable;
 
     // Vary the duty cycle using the debounced buttons
     always @(posedge clk) begin
-        if (duty_inc && DUTY_CYCLE < 9)
+        if (increase_debounced && DUTY_CYCLE < 9)
             DUTY_CYCLE <= DUTY_CYCLE + 1;  // Increase duty cycle by 10%
-        else if (duty_dec && DUTY_CYCLE > 1)
+        else if (decrease_debounced && DUTY_CYCLE > 1)
             DUTY_CYCLE <= DUTY_CYCLE - 1;  // Decrease duty cycle by 10%
     end
 
@@ -64,3 +50,4 @@ module pwmgenerator
     assign PWM_OUT = (counter_PWM < DUTY_CYCLE);
 
 endmodule
+
